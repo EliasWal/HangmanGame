d7ef5803c1b5a114e74e350bd5864de12a17eba6
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-15 00:07:02 +0200

    Send to Server Input Word and definition
    Client 1 mostly done

--- client1.py
+++ client1.py
@@ -1,4 +1,20 @@
 import socket,time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1",1234))
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("127.0.0.1",1234))
+	
+	# Send to server required infos
+	q1 = s.recv(1024).decode()
+	print(q1)
+	word = input()
+	s.send(word.encode())
+	
+	q2 = s.recv(1024).decode()
+	print(q2)
+	definition = input()
+	s.send(definition.encode())
+
+
+if __name__ == "__main__":
+	main()
