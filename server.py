21160cffb3fa35475987bb9d6a6cf1745e53a9ce
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-14 23:18:50 +0200

    Update server.py to accept 2 clients

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("127.0.0.1",1234))
-s.listen(1)
-(connection, address) = s.accept()
-print ("Connectd address:",address);
-while True:
-	data = connection.recv(100).decode("UTF-8")
-	if not data: break
-	print("Received: ",data)
-	if "exit" in data: break
-connection.close()
-print ("Server closed")
+import socket, time
+
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("127.0.0.1",1234))
+	s.listen()
+	
+	print("Waiting for first client to connect")
+	(client1, address1) = s.accept()
+	print("Client 1 connected at address:",address1);
+	
+	print("Waiting for second client to connect")
+	(client2, address2) = s.accept()
+	print("Client 2 connected at address:", address2);
+	
+	client1.close();
+	client2.close();
+	s.close();
+	print("Server closed")
+
+if __name__ == "__main__":
+	main()
