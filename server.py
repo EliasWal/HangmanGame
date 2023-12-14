af8b0ab1af99d7beb1a4bce3607413ddc84ac1f4
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-15 00:07:42 +0200

    Receive from client1 the word and def

--- server.py
+++ server.py
@@ -7,15 +7,24 @@ def main():
 	
 	print("Waiting for first client to connect")
 	(client1, address1) = s.accept()
-	print("Client 1 connected at address:",address1);
+	print("Client 1 connected at address:",address1)
+	
+	client1.sendall("Word:".encode())
+	word = client1.recv(1024)
+	print("Word:",word.decode())
+	
+	client1.sendall("Definition:".encode())
+	definition = client1.recv(1024)
+	print("Definition:",definition.decode())
 	
 	print("Waiting for second client to connect")
 	(client2, address2) = s.accept()
-	print("Client 2 connected at address:", address2);
+	print("Client 2 connected at address:", address2)
 	
-	client1.close();
-	client2.close();
-	s.close();
+	
+	client1.close()
+	client2.close()
+	s.close()
 	print("Server closed")
 
 if __name__ == "__main__":
