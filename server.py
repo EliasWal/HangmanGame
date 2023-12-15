a49c011bf5a8198f1ebee6545f539b012ce04fdc
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-16 01:30:56 +0200

    Update hangman to update the word after recv a guess
    the server sends to client 2 the definition and receives from him the guess, after he process the remaining letters left and no of attempts

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
 import socket, time
 
 def hangman(word_given, def_given, cl1, cl2):
-	print(word_given, def_given)
 	guessed = ["_"] * len(word_given)
 	attempts_left = 6
 	print(f"Word: {word_given}")
 	print(f"Definition: {def_given}")
+	cl2.sendall(f"Definition: {def_given}".encode())
 	
+	while "_" in guessed and attempts_left > 0:
+		aux = " ".join(guessed)
+		# cl1.sendall(f"{aux} {attempts_left}".encode())
+		cl2.sendall(f"{aux} {attempts_left}".encode())
+
+		guess = cl2.recv(1024).decode().lower()
+		print(guess)
+		
+		if guess in word_given:
+			for i in range(len(word_given)):
+				if word_given[i] == guess:
+					guessed[i] = guess
+		else:
+			attempts_left = -1
 
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,19 +31,20 @@ def main():
 	(client1, address1) = s.accept()
 	print("Client 1 connected at address:",address1)
 	
-	client1.sendall("Word:".encode())
-	word = client1.recv(1024)
-	print("Word:",word.decode())
-	
-	client1.sendall("Definition:".encode())
-	definition = client1.recv(1024)
-	print("Definition:",definition.decode())
-	
 	print("Waiting for second client to connect")
 	(client2, address2) = s.accept()
 	print("Client 2 connected at address:", address2)
 	
-	hangman(word,definition,address1,address2)
+	client1.sendall("Word:".encode())
+	word = client1.recv(1024).decode()
+	print("Word:",word)
+	
+	client1.sendall("Definition:".encode())
+	definition = client1.recv(1024).decode()
+	print("Definition:",definition)
+	
+	
+	hangman(word,definition,client1,client2)
 	
 	client1.close()
 	client2.close()
