7e3e4d766d8e3323adad78f327117aa79d7328e5
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-16 01:28:30 +0200

    Update client2.py to send guessings to server
    Client2 receives definition from server and then the number of choices left plus the left letters in the words. He will send the letter to the server then

--- client2.py
+++ client2.py
@@ -1,4 +1,24 @@
 import socket,time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1",1234))
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("127.0.0.1",1234))
+	
+	
+	while True:
+		response = s.recv(1024).decode()
+		print(response)
+		
+		response = s.recv(1024).decode()
+		print(response)
+
+		if "You won!" in response or "You lost!" in response:
+			break
+			
+		input_char = input("Enter the next letter: ")
+		s.sendall(input_char.encode())
+		
+	s.close()
+	
+if __name__ == '__main__':
+    main()
