60ff26bba687ea270ac623274820737dd42d7a26
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-21 01:52:32 +0200

    Delete unnecessary lines

--- client2.py
+++ client2.py
@@ -1,24 +1,24 @@
-import socket,time
+import socket, time
 
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(("127.0.0.1",1235))
+	s.connect(("127.0.0.1", 1235))
 	
-	#Receive definition from server
+	# Receive definition from server
 	response = s.recv(1024).decode()
 	print(response)
 	
 	while True:
 		response = s.recv(1024).decode()
-		print("Status: " , response)
-
+		print("Status: ", response)
+		
 		if "You won!" in response or "You lost!" in response:
 			break
-			
+		
 		input_char = input("Enter the next letter: ")
 		s.sendall(input_char.encode())
 		
 	s.close()
-	
+
 if __name__ == '__main__':
-    main()
+	main()
